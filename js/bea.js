20a81c3b97a05938baa8e50d0126c620a8312b26
Yu-Jisoo <s_ol_y@naver.com>
2026-01-23 08:18:05 +0900

    icon test

--- js/bea.js
+++ js/bea.js
@@ -134,18 +134,18 @@ function beaSlider(target,maxsV,slideW,slideM,pagerV,conV,pagerC,adapHVal){
         moveSlides: 1
     });
 }
-function toggleWish(){
-    var toggleStatus = false;
+// function toggleWish(){
+//     var toggleStatus = false;
 
-    $(".wishIcon").click(function(){
-        toggleStatus = !toggleStatus;
-        if(toggleStatus == true){
-            $(this).css("background-image","url('../images/icon_toggleWish_on.png')");
-        }else{
-            $(this).css("background-image","url('../images/icon_toggleWish_off.png')");
-        }
-    });
-}
+//     $(".wishIcon").click(function(){
+//         toggleStatus = !toggleStatus;
+//         if(toggleStatus == true){
+//             $(this).css("background-image","url('../images/icon_toggleWish_on.png')");
+//         }else{
+//             $(this).css("background-image","url('../images/icon_toggleWish_off.png')");
+//         }
+//     });
+// }
 function toggleCart(){
     $(".cartIcon").click(function(){
         $(this).css("background-image","url('../images/icon_toggleCart_on.png')");
